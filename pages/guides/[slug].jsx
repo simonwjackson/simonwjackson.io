0004e099faaae9abb88b36ae9390fe82bcffef06
Simon W. Jackson <simon@simonwjackson.com>
2020-09-04 20:52:00 -0500

    feat: load by slug

---
+++ pages/guides/[slug].jsx
@@ -0,0 +1,48 @@
+import fs from 'fs'
+import React from 'react'
+import path from 'path'
+import dynamic from 'next/dynamic'
+
+const Article = ({ filename, ...props}) => {
+  const { 
+      default: Content, 
+      meta 
+  } = require('../../' + filename)
+
+  return (<>
+    <h1>BOO!</h1>
+    <Content />
+  </>)  
+}
+
+export async function getStaticProps({ params }) {
+    const filename = path.join("guides", params.slug, "index.mdx")
+    // const frontmatter = matter(mdxPost)
+    console.log('the params',   params)
+
+    return {
+        props: {
+            filename
+        },
+    }
+}
+
+export async function getStaticPaths() {
+    const postsDirectory = path.join(process.cwd(), 'guides')
+    const mdxFiles = fs.readdirSync(postsDirectory)
+    console.log('the queried pages', mdxFiles)
+
+    // Loop through all post files and create array of slugs (to create links)
+    const paths = mdxFiles.map(filename => ({
+        params: {
+            slug: filename.replace(".mdx", "")
+        }
+    }));
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export default Article
